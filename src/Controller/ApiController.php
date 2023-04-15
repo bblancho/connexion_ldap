d707993ff188f3fd586f41b530d7e601731cc379
blancho <blanchard.banyingela@gmail.com>
2023-04-15 02:04:37 +0200

    Fin création api

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiController extends AbstractController
+{
+
+    public function __construct(private HttpClientInterface $httpClient)
+    {
+    }
+
+    #[Route('/api', name: 'app_api')]
+    public function index(): Response
+    {
+        $url     = 'https://api.github.com/users/bblancho/repos';
+        $method  = 'GET';
+        $options =
+        [
+            'query' => [
+                'sort' => 'name', // champs json ('id','description'...)
+                'direction'=> 'asc'
+            ],
+        ];
+
+        $response = $this->httpClient->request(
+            'GET',
+            $url ,
+            $options
+        );
+
+        // Get Content
+        $content = $response->getContent();
+        $reposGit = $response->toArray();
+
+        $statusCode = $response->getStatusCode();
+        // $statusCode = 200
+
+        $contentType = $response->getHeaders()['content-type'][0];
+        // $contentType = 'application/json'
+
+        return $this->render('api/index.html.twig', compact(
+            'reposGit',
+        ));
+    }
+
+    #[Route('/api/show/{id}', name: 'app_api_show')]
+    public function show($id): Response
+    {
+        //https://api.github.com/repos/OWNER/REPO
+        $url     = "https://api.github.com/repositories/{$id} "; 
+        $method  = 'GET';
+
+        $response = $this->httpClient->request(
+            'GET',
+            $url ,
+        );
+
+        $statusCode = $response->getStatusCode() ;
+
+        if(  $statusCode == Response::HTTP_NOT_FOUND )
+        {
+            throw new NotFoundHttpException("No response") ;
+        }
+
+        // Get Content
+        $content = $response->getContent();
+        $reposGit = $response->toArray();
+
+        
+
+        $contentType = $response->getHeaders()['content-type'][0];
+
+        return $this->render('api/show.html.twig', compact(
+            'reposGit',
+        ));
+    }
+
+    
+}
